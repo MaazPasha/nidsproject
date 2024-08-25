329b67ab6f99e1531a5cce3026da709c89faf125
MaazPasha <109347935+MaazPasha@users.noreply.github.com>
2024-08-26 04:36:36 +0530

    Add files via upload

---
+++ NidsApplication.java
@@ -0,0 +1,11 @@
+// src/main/java/NidsApplication.java
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NidsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NidsApplication.class, args);
+    }
+}
